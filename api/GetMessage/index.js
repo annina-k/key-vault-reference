101ae03fe8fde8a3d203cb38a6fa25984277083e
annina-k <37171540+annina-k@users.noreply.github.com>
2024-04-01 17:18:35 -0700

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,7 +1,9 @@
 module.exports = async function (context, req) {
-  string settingValue = Environment.GetEnvironmentVariable("TEST_KEY_VAULT_REFERENCE");
+  // Retrieve the value of the environment variable
+  const settingValue = process.env.TEST_KEY_VAULT_REFERENCE;
+
   context.res = {
     // status: 200, /* Defaults to 200 */
-    body: { text: "Super secret value is:" + settingValue },
+    body: { text: "Super secret value is: " + settingValue },
   };
 };
